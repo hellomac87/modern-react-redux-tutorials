0f821d6e565a7abb3182e0233934b122a1d24c1b
hellomac87 <hellomac87@gmail.com>
2018-09-14 20:18:07 +0900

    스테이트 더 파고들기

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
     render(){
         return (
             <div>
-                <input onChange={event => console.log(event.target.value)} type="text" placeholder="text" />
+                <input onChange={(event) => this.setState({ term : event.target.value})} type="text" placeholder="text" />
             </div>
         );
     }
