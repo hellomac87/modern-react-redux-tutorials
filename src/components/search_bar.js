e8da96061e9721a7ae2f238edc4a92fa1e7f564c
hellomac87 <hellomac87@gmail.com>
2018-09-14 19:55:12 +0900

    state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 // input 컴포넌트는 다른 컴포넌트들에게 몇가지 정보를 알려야 한다.
 // 그렇기에 클래스 기반의 컴포넌트로 만들어야 한다.
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            term: '',
+        }
+    }
     render(){
         return (
             <div>
