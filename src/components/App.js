7a9b0161ec8883967fdf19ae1391659522aa0f39
hellomac87 <hellomac87@gmail.com>
2018-09-17 20:49:03 +0900

    Props

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from "./video_list";
+
 const API_KEY = "AIzaSyD_ymZFHjd1-k11mirmA4dbR4G5HjDlvIU"; 
+
 
 // 유저가 새 검색어를 입력할 때마다, 새 검색 프로세스를 통해 스테이트의 검색 결과를 세팅해야 한다.
 
@@ -14,18 +17,16 @@ class App extends Component {
     }
 
     YTSearch({ key: API_KEY, term: 'Sam Smith' }, (videos) => {
-      //console.log(data);
       this.setState({
         videos, // === videos: videos
       });
     });
-
-    console.log(this.state.videos);
   }
   render() {
     return (
       <div>
         <SearchBar/>
+        <VideoList videos={this.state.videos}/>
         starter
       </div>
     );
---
+++ src/components/video_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const VideoList = (props) => {
+    // const videos = props.videos;
+    return (
+        <ul className="col-md-4 list-group">
+            {
+                props.videos.length ? props.videos.length : 'loading...'
+            }     
+        </ul>
+    );
+};
+
+export default VideoList;
