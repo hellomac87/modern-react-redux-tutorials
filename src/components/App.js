ea8475a7632a4729e27caaf927265d546d9182d3
hellomac87 <hellomac87@gmail.com>
2018-09-17 20:19:42 +0900

    유투브 검색 반환

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
-import youtube_api from 'youtube-api-v3-search';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = "AIzaSyD_ymZFHjd1-k11mirmA4dbR4G5HjDlvIU"; 
+
+YTSearch({key: API_KEY, term: 'Sam Smith'}, function(data){
+  console.log(data);
+});
 
 class App extends Component {
   render() {
